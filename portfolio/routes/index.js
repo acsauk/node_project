0b529b0b28c060da8c507d2ade7935d9b6137db1
Alex Saunders <acsauk@gmail.com>
2017-05-27 17:18:31 +0100

    creates storeController to separate controller logic from model logic. Creates index.pug to render the view referenced in the controller

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -1,19 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  // res.send('Hey! It works!');
-  res.render('hello', {
-    name: 'Alex',
-    cat: 'Tustin',
-    title: 'Test Title'
-  });
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.get('/', storeController.homePage)
 
 module.exports = router;

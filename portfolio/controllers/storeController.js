bcbb4db296b7ac4f2dba262563c16a4aea5e9d3f
Alex Saunders <acsauk@gmail.com>
2017-05-28 23:15:39 +0100

    refactors asaync await in createStore to happen during the store const creation in order to access properties of store. Changes redirect to go to the newly created store show page by interpolating the store slug into the address

--- portfolio/controllers/storeController.js
+++ portfolio/controllers/storeController.js
@@ -11,8 +11,8 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
+  const store = await(new Store(req.body)).save();
   await store.save();
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
-  res.redirect('/');
+  res.redirect(`/store/${store.slug}`);
 }
